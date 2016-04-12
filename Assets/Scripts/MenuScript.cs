85c840fc32448bf5c89fa32a2dee2f0724814daa
David Azouz <david.azouz@ad.aie.edu>
2016-04-12 10:45:07 +1000

    + Credits added + Code refactor - Panels

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -9,8 +9,8 @@
 /// 
 /// ***EDIT***
 /// - Menu and related systems working 	- David Azouz 11/04/16
-/// - Remaining comments added 		- David Azouz 
-/// - score added 					- David Azouz
+/// - Remaining comments added 		- David Azouz 11/04/16
+/// - Credits added					- David Azouz 12/04/16
 /// 
 /// </summary>
 
@@ -25,11 +25,13 @@ public class MenuScript : MonoBehaviour
     // PUBLIC VARIABLES
     //----------------------------------
     public GameObject controlsPanel; //Store a reference to the Game Object ControlsPanel
+    public GameObject creditsPanel; //Store a reference to the Game Object ControlsPanel
 
     //----------------------------------
     // PRIVATE VARIABLES
     //----------------------------------
     private bool isControls = false;
+    private bool isCredits = false;
 
     // Use this for initialization
     void Start ()
@@ -47,24 +49,35 @@ public class MenuScript : MonoBehaviour
             ToggleControlsView();
             //Debug.Log("isCon: " + isControls);
         }
-        // if Controls button is clicked...
-        if (isControls)
-        {
-            //... show the controls panel
-            controlsPanel.SetActive(isControls);
-        }
-        else
-        {
-            //... else don't display the controls panel
-            controlsPanel.SetActive(false);
-        }
 	}
 
-    //Shows/ hides the 'controls' panel
+    #region Panels
+    ///-----------------------------------
+    /// <summary> 
+    /// Shows/ hides the 'controls' panel
+    /// </summary>
+    ///-----------------------------------
     public void ToggleControlsView()
     {
+        // toggle controls true/ false
         isControls = !isControls;
+        // set the controls panel on/ off
+        controlsPanel.SetActive(isControls);
     }
+
+    ///-----------------------------------
+    /// <summary> 
+    /// Shows/ hides the 'credits' panel
+    /// </summary>
+    ///-----------------------------------
+    public void ToggleCreditsView()
+    {
+        // toggle credits true/ false
+        isCredits = !isCredits;
+        // set the credits panel on/ off
+        creditsPanel.SetActive(isCredits);
+    }
+    #endregion
 
     // Load the level to play the game
     public void LoadLevel()
