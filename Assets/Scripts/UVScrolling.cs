cb5bc50fffbc839cf0ba01595c54134d73d4c64e
Joel Hadfield <joel.hadfield@ad.aie.edu>
2016-04-12 14:47:43 +1000

    + Added Slime Planes + Added Ground and Texture + Added Bomb TImer + Added Bomb Transferring + Added Change State Functions + Audio + Slime Offset

--- Assets/Scripts/UVScrolling.cs
+++ Assets/Scripts/UVScrolling.cs
@@ -2,24 +2,25 @@
 using System.Collections;
 
 public class UVScrolling : MonoBehaviour {
-    public Shader r_ShaderSlime;
     public MeshRenderer r_MeshSlime;
+    public float uiTilingX = 25.0f; 
+
     // Use this for initialization
     void Start () {
-        r_ShaderSlime = GetComponent<Shader>();
+        //r_ShaderSlime = GetComponent<Shader>();
+        r_MeshSlime = GetComponent<MeshRenderer>();
     }
-	
-	// Update is called once per frame
-	void Update () {
-        //r_ShaderSlime.
-        for (uint i = 0; i < 100; ++i)
-        {
-            r_MeshSlime.material.SetTextureOffset("_MainTex", new Vector2(i, 0));
-            //if (i > 100)
-            //{
-            //    i--;
-            //}
-        }
 
+    // Update is called once per frame
+    void Update () {
+        Time.timeScale = 0.03f;
+        uiTilingX += Time.deltaTime * Time.timeScale;
+        //Debug.Log(uiTilingX);
+        r_MeshSlime.sharedMaterial.SetTextureOffset("_MainTex", new Vector2(uiTilingX, 25));
+        if (uiTilingX >= 100)
+        {
+            uiTilingX = 0;
+        }
+        Time.timeScale = 1f;
     }
 }
