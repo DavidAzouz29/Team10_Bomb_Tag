8b5b4cc5a32faa043f9062b3e6f89d2527437147
Joel Hadfield <joel.hadfield@ad.aie.edu>
2016-04-12 19:18:37 +1000

    + New Menu + Level Decorations + Bomb Particles

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+
+
+public class PlayButton : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+	
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
---
+++ Assets/Scripts/QuitButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class QuitButton : MonoBehaviour
+{
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Application.Quit();
+        }
+    }
+
+    /*void OnMouseEnter()
+    {
+        GetComponent<Light>().enabled = true;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<Light>().enabled = false;
+    }*/
+}
